b04902ccd0f9db86bf681b421753c966bf020248
n0k8t <timat-ti@mail.ru>
2017-11-24 19:41:30 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# lab13
+## Laboratory work XIII
+
+### Задание
+
+Написать программы на **C++** для сериализации и десериализации структуры `Person`.
+
+Структура `Person` определяется следующим образом:
+
+```cpp
+struct Email {
+  std::string nickname;
+  std::string server;
+};
+
+struct Person {
+  std::string  first_name;
+  std::string  last_name;
+  Email        email;
+  size_t       age;
+  std::string  phone;
+};
+```
---
+++ _build/CMakeFiles/pack.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/sergejtyrin/n0k8t/workspace/projects/lab13/sources/pack.cpp" "/Users/sergejtyrin/n0k8t/workspace/projects/lab13/_build/CMakeFiles/pack.dir/sources/pack.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/sergejtyrin/.hunter/_Base/f411b04/a5fd140/94c4b72/Install/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
